5d008f65c78d8502ffb5faf96636777e504259a4
田中秀和 <tanakahidekazu@tanakahidekazunoMac-mini.local>
2020-02-14 22:42:56 +0900

    implemnt eventsShow

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,14 +1,22 @@
 import _ from 'lodash';
 import { 
+    CREATE_EVENT,
     READ_EVENTS,
-    DELETE_EVENT
+    READ_EVENT,
+    DELETE_EVENT,
+    UPDATE_EVENT
 } from '../actions';
 
 export default (events = {}, action) => {
     switch (action.type) {
+        case CREATE_EVENT:
+        case READ_EVENT:
+        case UPDATE_EVENT:
+            const data = action.response.data;
+            return { ...events, [data.id]: data }
         case READ_EVENTS:
             return _.mapKeys(action.response.data, 'id');
-        case READ_EVENTS:
+        case DELETE_EVENT:
             delete events[action.id];
             return { ...events };
         default:
